530d510df72c7e7073198cb24a7f04ecf7a0d55b
thai-duong2003 <thaiq9577@gmail.com>
2023-07-12 17:05:19 +0700

    fixlogo

--- assests/js/register.js
+++ assests/js/register.js
@@ -1,140 +1,128 @@
-const registerBlock = document.querySelector('#register')
-const registerNext = document.querySelector('#register__next')
-const logIn = document.querySelector('#login')
-const formLogIn = document.querySelector('.auth-form__container--login')
-const btnLogInBlock = document.querySelector('.auth-form__side--has-acc-login')
-const formRegister = document.querySelector('.auth-form__container')
+const registerBlock = document.querySelector("#register");
+const registerNext = document.querySelector("#register__next");
+const logIn = document.querySelector("#login");
+const formLogIn = document.querySelector(".auth-form__container--login");
+const btnLogInBlock = document.querySelector(".auth-form__side--has-acc-login");
+const formRegister = document.querySelector(".auth-form__container");
 
-btnLogInBlock.addEventListener('click',function(e){
-    e.preventDefault();
-    formRegister.classList.add('hidden')
-    formLogIn.classList.remove('hidden')
-})
+btnLogInBlock.addEventListener("click", function (e) {
+  e.preventDefault();
+  formRegister.classList.add("hidden");
+  formLogIn.classList.remove("hidden");
+});
 
-var emailInput = document.querySelector('#email');
-var passwordInput = document.querySelector('#password');
-var confirmPasswordInput = document.querySelector('#confirm-password')
+var emailInput = document.querySelector("#email");
+var passwordInput = document.querySelector("#password");
+var confirmPasswordInput = document.querySelector("#confirm-password");
 
 function showError(input, message) {
-    var parent = input.parentElement;
-    var small = parent.querySelector('small');
-  
-    parent.classList.add('error');
-    small.innerText = message;
-  }
-  
-  function showSuccess(input) {
-    var parent = input.parentElement;
-    var small = parent.querySelector('small');
-  
-    parent.classList.remove('error');
-    small.innerText = '';
-  }
+  var parent = input.parentElement;
+  var small = parent.querySelector("small");
 
-  function checkEmptyError(listInput) {
-    var isEmptyError = false;
-    listInput.forEach(function(input) {
-      input.value = input.value.trim();
-      if (!input.value) {
-        isEmptyError = true;
-        showError(input, 'không được để trống');
-      } else {
-        showSuccess(input);
-      }
-    });
-    return isEmptyError;
-  }
-  
-  function checkEmailError(input) {
-    var regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  
-    var emailValue = input.value.trim();
-  
-    var isEmailError = !regexEmail.test(emailValue);
-  
-    if (regexEmail.test(emailValue)) {
-      showSuccess(input);
-    } else {
-      showError(input, 'Email không hợp lệ');
-    }
-  
-    return isEmailError;
-  }
-  
-  function checkLengthError(input, min, max) {
-    input.value = input.value.trim();
-    if (input.value.length < min) {
-      showError(input, 'phải có ít nhất ' + min + ' ký tự');
-      return true;
-    }
-    if (input.value.length > max) {
-      showError(input, 'không được quá ' + max + ' ký tự');
-      return true;
-    }
-    return false;
-  }
-  
-  function checkMatchPasswordError(passwordInput, cfpasswordInput) {
-      passwordInput.value = passwordInput.value.trim();
-      if (passwordInput.value !== cfpasswordInput.value) {
-        showError(cfpasswordInput, 'Mật khẩu không trùng khớp');
-        return true;
-      } 
-      return false;
+  parent.classList.add("error");
+  small.innerText = message;
 }
 
+function showSuccess(input) {
+  var parent = input.parentElement;
+  var small = parent.querySelector("small");
 
-registerBlock.addEventListener('submit', function(e) {
-    e.preventDefault();
-  
-    let isEmptyError = checkEmptyError([emailInput]);
-    let isEmailError = checkEmailError(emailInput);
-  
-    if (!isEmptyError && !isEmailError) {
-      registerBlock.classList.add('hidden');
-      registerNext.style.display = 'block';
+  parent.classList.remove("error");
+  small.innerText = "";
+}
+
+function checkEmptyError(listInput) {
+  var isEmptyError = false;
+  listInput.forEach(function (input) {
+    input.value = input.value.trim();
+    if (!input.value) {
+      isEmptyError = true;
+      showError(input, "không được để trống");
     } else {
-      console.log('Có lỗi xảy ra trong quá trình xác thực form');
+      showSuccess(input);
     }
   });
+  return isEmptyError;
+}
 
-registerNext.addEventListener('submit', function(e) {
-    e.preventDefault();
-  
-    let isPasswordLengthError = checkLengthError(passwordInput, 3, 10);
-    let isMatchError = checkMatchPasswordError(passwordInput, confirmPasswordInput);
-  
-    if (!isPasswordLengthError && !isMatchError) {
-        window.location.href = 'index.html';
-    } else {
-      console.log('Có lỗi xảy ra trong quá trình xác thực form');
-    }
-  });
+function checkEmailError(input) {
+  var regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-  logIn.addEventListener('submit', function(e) {
-    e.preventDefault();
-  
-    let isEmptyError = checkEmptyErrorcheckEmptyError([emailInput, passwordInput]);
-    let isEmailError = checkEmailError(emailInput);
-    let isPasswordLengthError = checkLengthError(passwordInput, 3, 10);
-   
-  
-    if (!isEmptyError && !isEmailError && !isPasswordLengthError) {
-        window.location.href = 'index.html';
-    } else {
-      console.log('Có lỗi xảy ra trong quá trình xác thực form');
-    }
-  });
+  var emailValue = input.value.trim();
 
+  var isEmailError = !regexEmail.test(emailValue);
 
+  if (regexEmail.test(emailValue)) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email không hợp lệ");
+  }
 
+  return isEmailError;
+}
 
+function checkLengthError(input, min, max) {
+  input.value = input.value.trim();
+  if (input.value.length < min) {
+    showError(input, "phải có ít nhất " + min + " ký tự");
+    return true;
+  }
+  if (input.value.length > max) {
+    showError(input, "không được quá " + max + " ký tự");
+    return true;
+  }
+  return false;
+}
 
+function checkMatchPasswordError(passwordInput, cfpasswordInput) {
+  passwordInput.value = passwordInput.value.trim();
+  if (passwordInput.value !== cfpasswordInput.value) {
+    showError(cfpasswordInput, "Mật khẩu không trùng khớp");
+    return true;
+  }
+  return false;
+}
 
+registerBlock.addEventListener("submit", function (e) {
+  e.preventDefault();
 
+  let isEmptyError = checkEmptyError([emailInput]);
+  let isEmailError = checkEmailError(emailInput);
 
+  if (!isEmptyError && !isEmailError) {
+    registerBlock.classList.add("hidden");
+    registerNext.style.display = "block";
+  } else {
+    console.log("Có lỗi xảy ra trong quá trình xác thực form");
+  }
+});
 
+registerNext.addEventListener("submit", function (e) {
+  e.preventDefault();
 
+  let isPasswordLengthError = checkLengthError(passwordInput, 3, 10);
+  let isMatchError = checkMatchPasswordError(
+    passwordInput,
+    confirmPasswordInput
+  );
 
+  if (!isPasswordLengthError && !isMatchError) {
+    window.location.href = "index.html";
+  } else {
+    console.log("Có lỗi xảy ra trong quá trình xác thực form");
+  }
+});
 
+logIn.addEventListener("submit", function (e) {
+  e.preventDefault();
 
+  let isEmptyError = checkEmptyError([emailInput, passwordInput]);
+  let isEmailError = checkEmailError(emailInput);
+  let isPasswordLengthError = checkLengthError(passwordInput, 3, 10);
+
+  if (!isEmptyError && !isEmailError && !isPasswordLengthError) {
+    window.location.href = "index.html";
+  } else {
+    console.log("Có lỗi xảy ra trong quá trình xác thực form");
+  }
+});
