9f2e89d6a53589dee99623cf77142d28f9c573ad
thai-duong2003 <135040412+thai-duong2003@users.noreply.github.com>
2023-07-13 10:32:54 +0700

    Update cart.js

--- assests/js/cart.js
+++ assests/js/cart.js
@@ -8,3 +8,25 @@ removeButtons.forEach(function (button) {
     frameElement.remove();
   });
 });
+
+const checkboxInputs = document.querySelectorAll('.checkbox_input');
+const totalPriceElement = document.querySelector('.wrap__footer-info-above-price');
+
+checkboxInputs.forEach(function(checkboxInput) {
+    checkboxInput.addEventListener('change', function() {
+        const quantityElement = this.closest('.wrap__item--frame').querySelector('.wrap__item--frame-voucher-product-number');
+        const quantity = parseInt(quantityElement.value);
+        const priceElement = this.closest('.wrap__item--frame').querySelector('.wrap__item--frame-voucher-product-price-new');
+        const price = parseFloat(priceElement.textContent.replace('đ', '').trim());
+        const currentPrice = parseFloat(totalPriceElement.textContent.replace('đ', '').trim());
+
+        
+        if (this.checked) {
+            const newPrice = currentPrice + (price * quantity);
+            totalPriceElement.textContent = 'đ' + newPrice.toFixed(3).replace(/\d(?=(\d{3})+\.)/g, '$&.').slice(0, -1);
+        } else {
+            const newPrice = currentPrice - (price * quantity);
+            totalPriceElement.textContent = 'đ' + newPrice.toFixed(3).replace(/\d(?=(\d{3})+\.)/g, '$&.').slice(0, -1);
+        }
+    });
+});
